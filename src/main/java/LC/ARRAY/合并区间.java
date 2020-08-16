6c6d40ec66d094469d55e77541573fdef401a079
fujiehhu <1163844624@qq.com>
2020-08-16 23:03:43 +0800

    2020年8月2日19点09分

--- src/main/java/LC/ARRAY/合并区间.java
+++ src/main/java/LC/ARRAY/合并区间.java
@@ -1,4 +1,4 @@
-package LC;
+package LC.ARRAY;
 
 import java.util.Arrays;
 
@@ -30,15 +30,15 @@ public class 合并区间 {
         Arrays.sort(intervals, (v1, v2) -> v1[0] - v2[0]);
         // 遍历区间
         int[][] res = new int[intervals.length][2];
-        int idx =0;
+        int idx = 0;
         for (int[] interval : intervals) {
             // 如果结果数组是空的，或者当前区间的起始位置 > 结果数组中最后区间的终止位置，
             // 则不合并，直接将当前区间加入结果数组。
-            if (idx ==0 || interval[0] > res[idx-1][1]) {
+            if (idx == 0 || interval[0] > res[idx - 1][1]) {
                 res[idx++] = interval;
             } else {
                 // 反之将当前区间合并至结果数组的最后区间
-                res[idx-1][1] = Math.max(res[idx-1][1], interval[1]);
+                res[idx - 1][1] = Math.max(res[idx - 1][1], interval[1]);
             }
         }
         return Arrays.copyOf(res, idx);
