edc2bac9235787d5e6b97365b21a77bbd0bedca3
fujiehhu <1163844624@qq.com>
2020-09-01 10:52:05 +0800

    17点10分

---
+++ src/main/java/BS/TC/Main.java
@@ -0,0 +1,13 @@
+package BS.TC;
+
+import java.util.Scanner;
+
+/**
+ * Created by fujie on 20/8/31.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+    }
+}
