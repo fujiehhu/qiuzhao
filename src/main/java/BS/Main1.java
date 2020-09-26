a13097881cb78718eb268de7c5620427ccc311e5
fujiehhu <1163844624@qq.com>
2020-09-26 18:47:39 +0800

    17点10分

---
+++ src/main/java/BS/Main1.java
@@ -0,0 +1,31 @@
+package BS;
+
+import javax.smartcardio.ATR;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Main1 {
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public Interval solve(int n, int k, String str1, String str2) {
+        // write code here
+        Interval res = new Interval();
+        if (k == str2.length()) {
+
+        }
+
+        return res;
+    }
+}
+
+class Interval {
+    int start;
+    int end;
+}
